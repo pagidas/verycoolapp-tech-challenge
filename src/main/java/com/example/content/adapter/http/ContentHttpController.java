590795cc6b16b4b51e9e76cd6496b10588264b2f
pagidas <pagakrivos@gmail.com>
2023-12-21 19:37:04 +0200

    Contracted two controllers into one
    We created two separate http controllers per operation exposed into
    `http`. We contract them into a single one as they form a single api,
    that of `content` domain.

--- src/main/java/com/example/content/adapter/http/ContentHttpController.java
+++ src/main/java/com/example/content/adapter/http/ContentHttpController.java
@@ -1,28 +1,38 @@
 package com.example.content.adapter.http;
 
+import com.example.content.core.domain.micro_types.ContentId;
 import com.example.content.core.domain.micro_types.ContentTitle;
 import com.example.content.core.domain.micro_types.UserId;
 import com.example.content.core.port.driver.CreateContent;
+import com.example.content.core.port.driver.ViewAllContent;
 import com.example.content.core.port.driver.model.CreateContentRequest;
+import com.example.content.core.port.driver.model.ViewContent;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.CompletedFileUpload;
-import jakarta.inject.Inject;
+import io.micronaut.serde.annotation.SerdeImport;
 
-@Controller
-public class CreateContentHttpController {
+import java.util.List;
+
+@Controller("/content")
+@SerdeImport(ViewContent.class)
+@SerdeImport(ContentId.class)
+@SerdeImport(ContentTitle.class)
+public class ContentHttpController {
 
     private final CreateContent createContent;
+    private final ViewAllContent viewAllContent;
 
-    @Inject
-    public CreateContentHttpController(CreateContent createContent) {
+    public ContentHttpController(CreateContent createContent, ViewAllContent viewAllContent) {
         this.createContent = createContent;
+        this.viewAllContent = viewAllContent;
     }
 
-    @Post(value = "/content/user/{userId}", consumes = MediaType.MULTIPART_FORM_DATA, produces = MediaType.TEXT_PLAIN)
+    @Post(value = "/user/{userId}", consumes = MediaType.MULTIPART_FORM_DATA, produces = MediaType.TEXT_PLAIN)
     HttpResponse<?> createContent(@PathVariable UserId userId, CompletedFileUpload file) {
         try {
             var request = new CreateContentRequest(
@@ -36,5 +46,11 @@ public class CreateContentHttpController {
         } catch (Exception e) {
             return HttpResponse.serverError(e.getMessage());
         }
+    }
+
+    @Get(value = "/user/{userId}", produces = MediaType.APPLICATION_JSON)
+    HttpResponse<List<ViewContent>> viewAllContent(@PathVariable UserId userId) {
+        var response = viewAllContent.invoke(userId);
+        return HttpResponse.ok(response);
     }
 }
